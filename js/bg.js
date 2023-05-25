df16f87d78deacd78fc141173cdbe1d458a8336b
SirKokos <102189180+SirKokos@users.noreply.github.com>
2023-05-25 15:29:17 +0300

    Add files via upload

--- js/bg.js
+++ js/bg.js
@@ -1,3 +1,22 @@
-function callAlert() {
-    alert('Кнопка была нажата');
+//дожидаемся полной загрузки страницы
+window.onload = function () {
+
+    //получаем идентификатор элемента
+    let open = document.getElementsByClassName('burger-menu')[0];
+    let close = document.getElementsByClassName('burger-menu-close')[0];
+    //вешаем на него событие
+    close.style.display = "none"
+    open.onclick = function() {
+        //производим какие-то действия
+        open.style.display = "none"
+        close.style.display = "flex"
+
+    }
+    close.onclick = function() {
+        //производим какие-то действия
+        open.style.display = "flex"
+        close.style.display = "none"
+       
+
+    }
 }
